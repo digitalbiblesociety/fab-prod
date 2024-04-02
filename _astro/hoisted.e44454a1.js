61a8a031dd53dba8979610f7fde646ece93a557c
Jon <jon@dbs.org>
2024-04-02 13:45:05 -0500

    auto-push

---
+++ _astro/hoisted.e44454a1.js
@@ -0,0 +1,1 @@
+import"./hoisted.77775e67.js";function i(e,t){e.value.length>t&&(e.value=e.value.substring(0,t))}const n=document.getElementById("message");n.addEventListener("input",function(){i(n,500)});
